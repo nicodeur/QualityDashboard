a0a951c0088062599e2c80c9a2ac23b8ff203b60
Nicolas Deblock <nico.deblock@gmail.com>
2017-08-10 16:41:37 +0200

    forgot file

---
+++ front/assets/js/core/plugin/CerberusGetLastInfo.class.js
@@ -0,0 +1,37 @@
+class CerberusGetLastInfo extends GetAndFillInfo {
+
+	constructor(globalSettings, contextData, contextModuleData) {
+		super();
+		this.globalSettings=globalSettings;
+		this.contextData=contextData;
+		this.contextModuleData = contextModuleData;
+	}
+
+	getUrl() {
+		return null;
+	}
+
+	getResult(msg) {
+		return null;
+	}
+
+    getInfo() {
+        let thisObject = this;
+        if (this.contextModuleData.module.cerberusPrefixTag != null) {
+            let cerberusTagInfo = new CerberusTagInfo(this.contextModuleData.module.cerberusPrefixTag, function (cerberusTags) {
+                    console.log(cerberusTags);
+                    let cerberusInfo = new CerberusInfo(thisObject.contextModuleData.selector, cerberusTags.lasttag);
+                    cerberusInfo.getInfo();
+                }
+            );
+            cerberusTagInfo.getInfo();
+        } else {
+            // hide
+            $(this.contextModuleData.selector + " [name='cerberusReport']").hide();
+        }
+	}
+
+    fillInfo() {
+		// do nothing
+    }
+}
